bd5cc738ede20648e6fa44c531f7961feb25ee71
Naceur <naceur@F4-A4-75-EC-44-7E.home>
2024-05-02 03:47:03 +0100

    Thu May  2 03:47:03 CET 2024

---
+++ src/Entity/Nurse.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NurseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NurseRepository::class)]
+class Nurse extends User
+{
+
+}
