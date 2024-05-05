000056fe7559763db224192f90b058f5bcd0b32a
Naceur <naceur@F4-A4-75-EC-44-7E.home>
2024-05-05 03:43:56 +0100

    Updates Bro!

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -6,6 +6,8 @@ use App\Repository\ActivityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request; // Add this line
+
 
 class ActivityController extends AbstractController
 {
@@ -29,6 +31,35 @@ class ActivityController extends AbstractController
         $activities = $this->activityRepository->findAllActivities();
         return $this->render('activity/showallactivities.html.twig', [
             'activities' => $activities,
+        ]);
+    }
+    #[Route('/activity/delete/{id}', name: 'activity_delete', methods: ['GET', 'POST'])]
+    public function delete(int $id, Request $request): Response
+    {
+        $activity = $this->activityRepository->find($id);
+        $this->activityRepository->deleteActivity($activity);
+
+        return $this->redirectToRoute('app_showallactivities');
+    }
+
+    #[Route('/activity/edit/{id}', name: 'activity_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, int $id): Response
+    {
+        $activity = $this->activityRepository->find($id);
+
+        if ($request->isMethod('POST')) {
+            $data = $request->request->all();
+
+            $activity->setDate(new \DateTime($data['activity_date']));
+            $activity->setStatus($data['activity_status']);
+
+            $this->activityRepository->updateActivity($activity);
+
+            return $this->redirectToRoute('app_showallactivities');
+        }
+
+        return $this->render('activity/edit_form.html.twig', [
+            'activity' => $activity,
         ]);
     }
 }
